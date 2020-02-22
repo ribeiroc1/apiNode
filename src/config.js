db28cf53469aacdaed6f4d34fdb04cac61ed5ccc
cleiton <cleiton.r@globomail.com>
2020-02-22 15:39:27 -0300

    Envio de email

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store !';
 
 module.exports = {
     connectionString: 'mongodb+srv://admin:admin@cluster0-yv52c.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridKey: 'TDB',
+    sendgridKey: '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',
     containerConnectionString: 'TDB'
 }
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -2,6 +2,8 @@
 
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
+const md5 = require('md5');
+const emailService = require('../services/email-service');
 
 exports.post = async (req, res, next) => {
     let contract = new ValidationContract();
@@ -17,7 +19,17 @@ exports.post = async (req, res, next) => {
     };
 
     try {
-        await repository.create(req.body)
+        await repository.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        });
+
+        emailService.send(
+            req.body.email,
+            'Bem vindo a Node Store',
+            global.EMAIL_TMPL.replace('{0}', req.body.name));
+
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso !'
         });
